53c5e597e13a1c084bbee1bda9111e3a7132e0f6
Olav Rorvik <olavr.dev@gmail.com>
2024-04-17 20:55:00 +0200

    create and show comments logic done

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,5 +1,9 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
 const commentsSectionElement = document.getElementById('comments');
+const commentsSectionParagraphElement = document.querySelector('#comments p');
+const commentsFormElement = document.querySelector('#comments-form form');
+const commentTitleElement = document.getElementById('title');
+const commentTextElement = document.getElementById('text');
 
 function createCommentsList(comments) {
   const commentListElement = document.createElement('ol');
@@ -23,9 +27,49 @@ async function fetchCommentsForPost(event) {
   const response = await fetch(`/posts/${postId}/comments`);
   const responseData = await response.json();
 
-  const commentsListElement = createCommentsList(responseData);
-  commentsSectionElement.innerHTML = '';
-  commentsSectionElement.appendChild(commentsListElement);
+  if (responseData && responseData.length > 0) {
+    const commentsListElement = createCommentsList(responseData);
+    commentsSectionElement.innerHTML = '';
+    commentsSectionElement.appendChild(commentsListElement);
+  } else {
+    commentsSectionElement.firstElementChild.textContent = 'We could not find any comments. Maybe add one?';
+  }
 }
 
+async function saveComment(event) {
+  event.preventDefault();
+  const postId = commentsFormElement.dataset.postId;
+
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+
+  const comment = { title: enteredTitle, text: enteredText };
+
+  const response = await fetch(`/posts/${postId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify(comment),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  fetchCommentsForPost();
+  commentTitleElement.value = '';
+  commentTextElement.value = '';
+}
+
+async function showNumberOfComments() {
+  const postId = loadCommentsBtnElement.dataset.postId;
+  const response = await fetch(`/posts/${postId}/comments`);
+  const responseData = await response.json();
+
+  if (responseData && responseData.length > 0) {
+    loadCommentsBtnElement.textContent = 'Load Comments (' + responseData.length + ')';
+  } else {
+    return;
+  }
+}
+
+showNumberOfComments();
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
+commentsFormElement.addEventListener('submit', saveComment);
