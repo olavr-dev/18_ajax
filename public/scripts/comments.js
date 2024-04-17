bfcce5c9c9948b4a455554e8d412e4ef4e99b612
Olav Rorvik <olavr.dev@gmail.com>
2024-04-17 21:18:26 +0200

    added error handling

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -4,6 +4,7 @@ const commentsSectionParagraphElement = document.querySelector('#comments p');
 const commentsFormElement = document.querySelector('#comments-form form');
 const commentTitleElement = document.getElementById('title');
 const commentTextElement = document.getElementById('text');
+const commentErrorElement = document.getElementById('comment-error');
 
 function createCommentsList(comments) {
   const commentListElement = document.createElement('ol');
@@ -24,15 +25,26 @@ function createCommentsList(comments) {
 
 async function fetchCommentsForPost(event) {
   const postId = loadCommentsBtnElement.dataset.postId;
-  const response = await fetch(`/posts/${postId}/comments`);
-  const responseData = await response.json();
+  try {
+    const response = await fetch(`/posts/${postId}/comments`);
 
-  if (responseData && responseData.length > 0) {
-    const commentsListElement = createCommentsList(responseData);
-    commentsSectionElement.innerHTML = '';
-    commentsSectionElement.appendChild(commentsListElement);
-  } else {
-    commentsSectionElement.firstElementChild.textContent = 'We could not find any comments. Maybe add one?';
+    if (!response.ok) {
+      commentErrorElement.textContent = 'Oops! Fetching comments failed!';
+      commentErrorElement.style.display = 'block';
+      return;
+    }
+    const responseData = await response.json();
+
+    if (responseData && responseData.length > 0) {
+      const commentsListElement = createCommentsList(responseData);
+      commentsSectionElement.innerHTML = '';
+      commentsSectionElement.appendChild(commentsListElement);
+    } else {
+      commentsSectionElement.firstElementChild.textContent = 'We could not find any comments. Maybe add one?';
+    }
+  } catch (error) {
+    commentErrorElement.textContent = 'Oops! Getting comments failed!';
+    commentErrorElement.style.display = 'block';
   }
 }
 
@@ -45,17 +57,27 @@ async function saveComment(event) {
 
   const comment = { title: enteredTitle, text: enteredText };
 
-  const response = await fetch(`/posts/${postId}/comments`, {
-    method: 'POST',
-    body: JSON.stringify(comment),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  try {
+    const response = await fetch(`/posts/${postId}/comments`, {
+      method: 'POST',
+      body: JSON.stringify(comment),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  fetchCommentsForPost();
-  commentTitleElement.value = '';
-  commentTextElement.value = '';
+    if (response.ok) {
+      fetchCommentsForPost();
+      commentTitleElement.value = '';
+      commentTextElement.value = '';
+    } else {
+      commentErrorElement.textContent = 'Oops! Server Error - Could not send comment!';
+      commentErrorElement.style.display = 'block';
+    }
+  } catch (error) {
+    commentErrorElement.textContent = 'Oops! Connection Error - Please try again later!';
+    commentErrorElement.style.display = 'block';
+  }
 }
 
 async function showNumberOfComments() {
